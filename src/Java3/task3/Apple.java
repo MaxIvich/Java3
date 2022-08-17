8e58844e6d8956a2bd8e723fc29e78bcb5ddb5f1
Maxivich <maxivlevich@gmail.com>
2022-08-17 20:25:38 +0300

    34

--- src/Java3/task3/Apple.java
+++ src/Java3/task3/Apple.java
@@ -2,5 +2,5 @@ package Java3.task3;
 
 public class Apple extends Fruit {
 
-    float appleWeight = 1.0f;
+    private final float appleWeight = 1.0f;
 }
--- src/Java3/task3/Fruit.java
+++ src/Java3/task3/Fruit.java
@@ -3,6 +3,9 @@ package Java3.task3;
 public abstract class Fruit {
 
 
+    
+
+
 
 
 
