62afb6f955d7f869aa572a2e886d5beda63cd539
Maxivich <maxivlevich@gmail.com>
2022-08-28 16:50:39 +0300

    34

---
+++ src/Java3/Hw4.java
@@ -0,0 +1,76 @@
+package Java3;
+
+public class Hw4  {
+
+
+
+
+
+    final public String A = "A";
+    final public String B = "B";
+    final public String C = "C";
+    private String ABC = C;
+
+
+    //  private static void printABC(){
+    //      PrintA();
+    //      PrintB();
+    //      PrintC();
+    //
+    //  }
+
+    public synchronized   void PrintA() {
+
+        if (ABC.equals(A) || ABC.equals(B)) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        if(ABC.equals(C)) {
+            ABC = A;
+            System.out.print(ABC);
+            notifyAll();
+        }
+
+
+    }
+
+    public synchronized void PrintB() {
+
+            if (ABC.equals(C) || ABC.equals(B)) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(ABC.equals(A)){
+            ABC = B;
+            System.out.print(ABC);
+                notifyAll();
+            }
+
+
+
+
+    }
+    public synchronized void PrintC() {
+
+        if (ABC.equals(A) || ABC.equals(C)) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(ABC.equals(B)) {
+                ABC = C;
+                System.out.print(ABC);
+                notifyAll();
+            }
+
+
+    }
+}
